e92c36bcca979f7fff4a6962063ac9677e668ee0
halex02 <halex02@msn.com>
2016-01-12 08:10:42 +0100

    MAJ du TPO2

---
+++ TPs/TP02/tp02.hs
@@ -0,0 +1,24 @@
+module TP02 where
+
+--Echauffement :
+--Question 1 :
+alterne []     = []
+alterne [x]    = [x]
+alterne (x:xs) = x:(alterne (tail xs))
+
+--Question 2 :
+combine f []     _      = []
+combine f _      []     = []
+combine f (x:xs) (y:ys) = (f x y):(combine f xs ys)
+
+--Triangle de Pascal :
+--Question 3 :
+pasPascal :: [Integer] -> [Integer]
+pasPascal [1] = [1,1]
+pasPascal xs  = (zipWith (+) xs (0:xs)) ++ [1] 
+
+--Question 4 :
+pascal = iterate pasPascal [1]
+
+--Courbe du dragon :
+--Voir fichier dragon.hs
