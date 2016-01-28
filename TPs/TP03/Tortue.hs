fc8a5f3c5a963d41174a14b7f8b30cd8dd70e915
halex02 <halex02@msn.com>
2016-01-28 15:32:50 +0100

    push interpreteSymbole

--- TPs/TP03/Tortue.hs
+++ TPs/TP03/Tortue.hs
@@ -28,23 +28,24 @@ symbolesTortue (_,_,_,_,s) = s
 avance :: Config -> EtatTortue -> EtatTortue
 avance c ((x,y),cap) = ((x',y'), cap)
   where
-  x'= x+d*cos(cap)
-  y'= y+d*sin(cap)
-  d = facteurEchelle c
+    x'= x+d*cos(cap)
+    y'= y+d*sin(cap)
+    d = facteurEchelle c
 
 tourneAGauche :: Config -> EtatTortue -> EtatTortue
 tourneAGauche c (p,cap) = (p, cap')
   where
-  cap' = cap + angle c
+    cap' = cap + angle c
 
 tourneADroite :: Config -> EtatTortue -> EtatTortue
 tourneADroite c (p,cap) = (p, cap')
   where
-  cap' = cap - angle c
+    cap' = cap - angle c
 
 filtreSymbolesTortue :: Config -> Mot -> Mot
 filtreSymbolesTortue c mot = filter (\x -> x `elem` (symbolesTortues c)) mot
 
 interpreteSymbole :: Config -> EtatDessin -> Symbole -> EtatDessin
-interpreteSymbole c (etatTortue, ) s = 
-
+interpreteSymbole c ((pt, dir), path) 'F' = 
+interpreteSymbole c (e, path) '+'         = ((tourneAGauche c e), path)
+interpreteSymbole c (e, path) '-'         = ((tourneADroite c e), path)
