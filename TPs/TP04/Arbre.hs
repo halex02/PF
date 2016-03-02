36a3f4c2a3a3303328b8a4e6518979ea86e5b4a6
kaal-dam <dam.toulouse@gmail.com>
2016-03-02 19:43:56 +0100

    arbre

--- TPs/TP04/Arbre.hs
+++ TPs/TP04/Arbre.hs
@@ -36,9 +36,51 @@ module Arbre where
   estComplet (Noeud c v gauche droit) = ((taille gauche) == (taille droit)) && (estComplet gauche) && (estComplet droit)
 
   prop_CompletPeigne xs = estComplet (peigneGauche xs)
+ 
   
-  
-  
+  splitEnt :: [(c,a)] -> ([(c,a)],(c,a),[(c,a)])
+  splitEnt l =  (xs,r,ys)
+    where xs = take ( (length l) `div` 2 ) l
+          r  = last (take (( (length l) `div`2 )+1) l)
+          ys = drop (( (length l) `div` 2 )) l
+      
 
 
+
+  complet :: Int -> [(c, a)] -> Arbre c a 
+  complet 0 [] = Feuille
+  complet x cs = Noeud (fst mid)  (snd mid)  (complet (x-1) gauche)  ( complet (x-1) droit)
+    where (gauche,mid,droit) = splitEnt cs
+
   
+      
+  infinit :: x -> [x]
+  infinit = iterate (\x -> x)
+
+  
+  create :: a -> ((),a)
+  create a = ((),a)
+  
+  listAllChar = ['a'..]
+  listAllCharTransf = [ (create x ) | x<-listAllChar ]
+  
+  aplatit :: Arbre c a -> [(c,a)]
+  aplatit Feuille = [] 
+  aplatit (Noeud c v gauche droit) = (aplatit gauche) ++ [(c,v)] ++ (aplatit droit)
+
+
+  noeud :: (c -> String) -> (a -> String) -> (c,a) -> String
+  noeud fc fa (c,a) = (fa a) ++ " [color="++(fc c)++", fontcolor="++(fc c)++"]" 
+  
+  listArcs :: Arbre c a -> Arbre c a -> Arbre c a -> [(a,a)]
+  listArcs _ Feuille Feuille = []
+  listArcs (Noeud _ v _ _) Feuille (Noeud _ v2 _ _) = [(v,v2)]  
+  listArcs (Noeud _ v _ _) (Noeud _ v2 _ _) Feuille = [(v,v2)]  
+  listArcs (Noeud _ v _ _) (Noeud _ v2 _ _) (Noeud _ v3 _ _)  =[(v,v2),(v,v3)] 
+
+  arcs :: Arbre c a -> [(a,a)]
+  arcs Feuille = [] 
+  arcs (Noeud c v gauche droit) = (listArcs (Noeud c v gauche droit) gauche droit)  ++ arcs gauche ++ arcs droit
+
+  arc :: (a -> String) -> (a,a) -> String
+  arc f (a1,a2) = (f a1) ++ " -> " ++(f a2 ) 
